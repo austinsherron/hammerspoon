7ff46185cb98a483d87bed0d27f7f81d8d55b97f
Austin Sherron <austin@pryvate.email>
2024-02-16 20:24:11 -0800

    [Fix] Update Mouse Move Bindings

--- keymap/mouse.lua
+++ keymap/mouse.lua
@@ -2,5 +2,14 @@ local Hotkey = require 'utils.ctrl.hotkey'
 local Mouse = require 'utils.ctrl.mouse'
 
 -- mouse bindings
-Hotkey.hotkey('h', Mouse.prev_screen, { 'shift', 'alt' })
-Hotkey.hotkey('l', Mouse.next_screen, { 'shift', 'alt' })
+Hotkey.hotkey(
+  'h',
+  { name = 'Mouse > prev screen', fn = Mouse.prev_screen },
+  { 'shift', 'alt' }
+)
+Hotkey.hotkey(
+  'l',
+  { name = 'Mouse > next screen', fn = Mouse.next_screen },
+  { 'shift', 'alt' }
+)
+
