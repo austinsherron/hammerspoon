06f8f86b828f125cf39fae3b277f18e1a423ea16
Austin Sherron <austin@pryvate.email>
2026-02-19 13:17:11 -0500

    [Update] Add Quake Keymap For Slack

--- keymap/quake.lua
+++ keymap/quake.lua
@@ -7,5 +7,6 @@ local QK = Quake.get()
 -- quake windows ---------------------------------------------------------------
 
 Hotkey.hotkey('F7', { name = 'Quake > Hide All', fn = Lambda.make(Quake.hide_all, QK) })
+Hotkey.hotkey('F9', { name = 'Quake > WhatsApp', fn = QK:for_binding 'Slack' })
 Hotkey.hotkey('F10', { name = 'Quake > WhatsApp', fn = QK:for_binding 'WhatsApp' })
 Hotkey.hotkey('F11', { name = 'Quake > iTerm', fn = QK:for_binding 'iTerm2' })
